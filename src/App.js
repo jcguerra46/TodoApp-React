713bf35be5bdc18a2968c37e43f31fd173bf4d52
Juan Carlos Guerra <juancarlosguerra46@gmail.com>
2018-09-29 14:14:55 -0300

    Update App.js
    ajustes del layout

--- src/App.js
+++ src/App.js
@@ -7,6 +7,10 @@ class App extends Component {
   render() {
     return (
       <div className="App">
+        <header className="App-header">
+          <img src={logo} className="App-logo" alt="logo" />
+          <h1 className="App-title">TodoApp React</h1>
+        </header>
         <TodoList/>
       </div>
     );
